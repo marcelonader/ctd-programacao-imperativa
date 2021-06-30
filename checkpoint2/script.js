0a9eef9b68f691f1a0dc9871e20bc66661d1090e
marcelonader <marcelo.m.nader@gmail.com>
2021-06-30 15:27:54 -0300

    desafio de metodos de array

--- checkpoint2/script.js
+++ checkpoint2/script.js
@@ -10,5 +10,5 @@ console.table(M.microondas.menuDePratos)
 M.microondas.executar('macarrão') //tempo padrão
 M.microondas.executar('macarrão', 5) //tempo insuficiente
 M.microondas.executar('macarrão', 17) //tempo acima do dobro
-M.microondas.executar('macarrão', 25) //tempo acima do triplo
+M.microondas.executar('carne', 25) //tempo acima do triplo
 M.microondas.executar('lasanha')
---
+++ modulo_2_javascript-avancado/metodos-array-avancados/metodos-de-arrays/metodos-de-arrays.js
@@ -0,0 +1,32 @@
+// 1. Crie um array de números pares, e utilizando a função .map() nesse array, crie um novo array com apenas números ímpares.
+
+let numPares = [2, 4, 6, 8, 10, 12, 14, 16, 18, 20];
+let numImpares = numPares.map(num => num + 1);
+console.log(numImpares);
+
+// 2. Crie um array de nomes, que possua dois índices com o nome Maria. Utilize o .filter() para obter apenas esses dois índices com o nome Maria.
+
+let nomes = ['Marcelo', 'Rita', 'Maria', 'Silvia', 'Christopher', 'Maria'];
+console.log(nomes.filter(elemento => elemento == 'Maria'));
+
+//ou
+
+// let nomes = ['Marcelo', 'Rita', 'Maria José', 'Silvia', 'Christopher', ' Ana Maria'];
+// console.log(nomes.filter(elemento =>  elemento.includes('Maria')));
+
+
+// 3. Crie um array de números e utilize a função .reduce() para devolver uma string com os números formatados.
+
+// Exemplo [1,5,9,3,7] => “1 – 5 – 9 – 3 – 7”
+
+let numeros = [10, 23, 7, 16, 48, 506];
+console.log(numeros.reduce((acumulador, elemento) => {
+    return `${acumulador} - ${elemento}`
+}));
+
+// 4. Crie um array de animais, após isso passe por cada índice utilizando o .forEach() e imprima a frase “O animal é NOME_DO_ANIMAL”.
+
+let animais = ['onça', 'macaco', 'cachorro', 'gato'];
+animais.forEach(animal => {
+    console.log(`O animal é ${animal.toUpperCase()}`)
+})
